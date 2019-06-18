5cc12a01e34f15a3197c58d1d8fe7f72132a1328
SupernaturalCow <inaniloquaint@gmail.com>
2019-06-19 08:56:58 +1000

    Better formatting, improved textbox.

--- functionality.js
+++ functionality.js
@@ -1,4 +1,6 @@
 
+var _text_content;
+
 // Load saved text on startup.
 window.onload = function() {
     load();
@@ -23,15 +25,18 @@ function load() {
     if(result === null) {
         result = "No note saved";
     }
-    document.getElementById('textbox').innerHTML = result;
+    with (document.getElementById('textbox')) {
+    	textContent = result;
+    	_text_content = textContent;
+	}
 }
 
 // Save the current text field as a note.
 function save() {
     if(check_web_storage_support() == true) {
         var textbox = document.getElementById("textbox");
-        if(textbox.innerHTML != '') {
-            localStorage.setItem("note", textbox.innerHTML);
+        if(textbox.textContent != '') {
+            localStorage.setItem("note", textbox.textContent);
         }
         else {
             alert("Nothing to save");
@@ -41,13 +46,29 @@ function save() {
 
 // Clear the current text field.
 function clear() {
-    document.getElementById('textbox').innerHTML = "";
+    document.getElementById('textbox').textContent = "";
 }
 
 // Load data from file.
 function load_data() {
 	fetch("data/notes.txt")  // Object promise.
 	.then( response => response.text() )  // Object response.
-	.then( data => document.getElementById('textbox').innerHTML = marked(data));
+	.then( data => document.getElementById('textbox').textContent = data);
 }
+
+// Apply formatting.
+function apply_formatting() {
+	with (document.getElementById('textbox')) {
+		_text_content = textContent;
+		innerHTML = marked(_text_content);
+	}
+}
+
+// Clear formatting.
+function clear_formatting() {
+	with (document.getElementById('textbox')) {
+		innerHTML = _text_content;
+	}
+}
+
 //*/

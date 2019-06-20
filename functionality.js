b8315d9ae42e9dd2225fa8f2b570652f4470c6d1
SupernaturalCow <inaniloquaint@gmail.com>
2019-06-20 22:55:29 +1000

    Switched converter from Marked to Showdown.

--- functionality.js
+++ functionality.js
@@ -1,9 +1,16 @@
 
-var _raw_text;
-
-// Load saved text on startup.
+// Window loading event.
 window.onload = function() {
+
+    // Load text.
     load();
+
+    // On keyup, save textContent to localStorage.
+    with (document.getElementById('textbox')) {
+        addEventListener( 'keyup', function() {
+            save();
+        });
+    } 
 }
 
 // Check the browser supports web storage.
@@ -20,25 +27,17 @@ function check_web_storage_support() {
 // Save the current text field as a note.
 function save() {
     if(check_web_storage_support() == true) {
-        var textbox = document.getElementById("textbox");
-        if(textbox.innerHTML != '') {
-            localStorage.setItem("note", textbox._raw_text);  // Save raw markdown only.
+        with (document.getElementById("textbox")) {
+            if (innerHTML != '') {
+                localStorage.setItem("note", innerHTML);  // Save as markdown only.
+            }
         }
     }
 }
 
 // Load the saved note.
 function load() {
-    if(check_web_storage_support() == true) {
-        result = localStorage.getItem('note');
-    }
-    if(result === null) {
-        result = "No note saved";
-    }
-    with (document.getElementById('textbox')) {
-        _raw_text = result;  // This is used to keep track of our markdown.
-    	innerHTML = result;
-	}
+    document.getElementById('textbox').innerHTML = localStorage.getItem("note");
 }
 
 // Clear the current text field.
@@ -48,26 +47,46 @@ function clear() {
 
 // Load data from file.
 function load_data() {
-	fetch("data/notes.txt")  // Object promise.
-	.then( response => response.text() )  // Object response.
-	.then( data => document.getElementById('textbox').innerHTML = data);  // Object value.
+    // Object promise.
+	fetch("data/notes.txt") 
+        // Object response.
+	   .then(function(response) {
+            return response.text();
+       })
+       // Object value.
+       .then(function(text) {
+            document.getElementById('textbox').innerHTML = text;
+       });
+
+    var _result = document.getElementById('textbox').innerHTML;
+    console.log("loaded note: "+_result);
 }
 
-// Apply formatting.
-function apply_formatting() {
+// Convert text to HTML formatting.
+function convert_html() {
 	with (document.getElementById('textbox')) {
-        console.log(innerHTML);
-        _raw_text = innerHTML;  // Remember this, before we fiddle with it.
-        clear_formatting();  // Clear any previous formatting, bar newlines.
-		innerHTML = marked(innerHTML);
-        console.log(innerHTML);
+        var _converter = new showdown.Converter(),
+		innerHTML = _converter.makeHtml(innerHTML);
+        console.log("converted to HTML: "+innerHTML);
 	}
 }
 
+// Convert text to Markdown formatting.
+function convert_markdown() {
+    with (document.getElementById('textbox')) {
+        var _converter = new showdown.Converter(),
+            _html_text = _converter.makeMarkdown(innerHTML);
+        innerHTML = _html_text;
+        console.log("converted to MD: "+innerHTML);
+    }
+}
+
+/*
+
 // Clear formatting.
 function clear_formatting() {
-	with (document.getElementById('textbox')) {
-		//innerHTML = textContent; // This can be better.
+    with (document.getElementById('textbox')) {
+        //innerHTML = textContent; // This can be better.
         innerHTML = innerHTML.replace(/<div>/g, "NNN");
         innerHTML = innerHTML.replace(/<br>/g, "NNN");
         innerHTML = innerHTML.replace(/\n/g, "NNN");
@@ -76,12 +95,6 @@ function clear_formatting() {
     }
 }
 
-// Return text cache
-function reset_text_to_markdown() {
-    with (document.getElementById('textbox')) {
-        innerHTML = _raw_text;
-        console.log(_raw_text);
-    }
-}
-
 //*/
+
+
